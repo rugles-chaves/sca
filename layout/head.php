40042caa22fe37f778813068b3539fdd23b28cf0
rugles-chaves <70105605+rugles-chaves@users.noreply.github.com>
2024-10-06 20:16:06 -0400

    inicio

---
+++ layout/head.php
@@ -0,0 +1,114 @@
+﻿
+<!DOCTYPE html>
+<html lang="en"> 
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo isset($title) ? $title : 'Título Padrão'; ?></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="./vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="./vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="./vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="./dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="./vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+      
+                
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <!-- <input type="text" class="form-control" placeholder="Search..."> -->
+                                <span class="input-group-btn">
+                                <!-- <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button> -->
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-user fa-fw"></i> Cadastro de Usaurios</a>
+                        </li>
+
+                        <li>
+                         <a href="#"><i class="fa fa-laptop fa-fw"></i> Registros</a>
+                        </li>
+
+                        <li>
+                         <a href="./vsu.php"><i class="fa fa-automobile fa-fw"></i> Visializador</a>
+                        </li>
+                            
+                         <li>
+                            <a href="#"><i class="fa fa-cog fa-fw"></i> Equipamentos<span class="fa arrow"></span></a>
+                           
+                            <ul class="nav nav-second-level">
+
+                                <li>
+                                 <a href="#"><i class="fa fa-user"></i> Leitor Facial</a>
+                                </li>
+                                <li>
+                                 <a href="acesso.php"><i class="fa fa-upload"></i> Leitor de Mesa</a>
+                                </li>
+                            
+                            </ul>
+
+                        </li>
+
+                        <li>
+                            <a href="control/sair.php"><i class="fa fa-power-off"></i> Sair</a>
+                        </li>
+                       
+                       
+                            <!-- /.nav-second-level -->
+                        </li>
+                       
+                        <li>
+                            
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+        
+    
